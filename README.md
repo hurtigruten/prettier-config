c78c9cb42303ccff6ebd02bda12e56a1212edc11
neonwarp <tibor.czegledy@hurtigruten.com>
2022-09-13 02:30:09 +0200

    Add config

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hurtigruten's Prettier Config
+
+```bash
+npm i -D @hurtigruten/prettier-config
+```
+
+Reference in your `package.json`
+
+```json
+{
+  
+  "name": "my-app",
+  "version": "0.1",
+  ...
+  "prettier": "@hurtigruten/prettier-config"
+}
+```
---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'auto',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleAttributePerLine: true
+};
